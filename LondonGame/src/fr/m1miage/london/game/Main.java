f91bba7358d1f9010aa2c85b39ffbc22fbdb4f9d
Aethia <elise.richard@live.fr>
2014-03-14 23:47:28 +0100

    Test d'ajout d'un nouveau projet avec libgdx..

---
+++ LondonGame/src/fr/m1miage/london/game/Main.java
@@ -0,0 +1,23 @@
+package fr.m1miage.london.game;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import fr.m1miage.london.db.CartesManager;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        System.out.println(CartesManager.getCartes());
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "LondonGame";
+		cfg.width = 1280;
+		cfg.height = 720;
+		
+		new LwjglApplication(new LondonGame(), cfg);
+	}
+
+}
