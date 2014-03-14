d97e461bd41da8a0b252141a71f844bca41ccfe3
Aethia <elise.richard@live.fr>
2014-03-14 23:43:29 +0100

    Test

--- London/src/fr/m1miage/london/London.java
+++ London/src/fr/m1miage/london/London.java
@@ -1,11 +1,7 @@
 package fr.m1miage.london;
 
-
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
 import fr.m1miage.london.db.CartesManager;
-import fr.m1miage.london.game.LondonGame;
+
 
 public class London {
 
@@ -13,12 +9,7 @@ public class London {
 		// TODO Auto-generated method stub
 
 	        System.out.println(CartesManager.getCartes());
-	        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-			cfg.title = "LondonGame";
-			cfg.width = 1280;
-			cfg.height = 720;
-			
-			new LwjglApplication(new LondonGame(), cfg);
+	        
 	}
 
 }
