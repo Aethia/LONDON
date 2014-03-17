8b661314db0d5ad0f624b7c4a6b298b1ef3d0c65
Aethia <elise.richard@live.fr>
2014-03-17 22:39:34 +0100

    Merge des branches classes et master. Conflicts: 	test.txt

---
+++ London/src/fr/m1miage/london/classes/Main.java
@@ -0,0 +1,26 @@
+package fr.m1miage.london.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	private List<Carte> lesCartes = new ArrayList<Carte>(); 
+	
+	public Main(){
+	}
+	
+	// ajouter une carte dans la main
+	public void ajouterCarte(Carte c){
+		lesCartes.add(c);
+	}
+
+	public List<Carte> getLesCartes() {
+		return lesCartes;
+	}
+
+	public int getNb_cartes() {
+		return lesCartes.size();
+	}
+	
+	
+}
