da27216a3f4a6626702e14cae57ca1de1729cb76
unknown <ethilanie@gmail.com>
2014-03-19 15:04:40 +0100

    Initialisation des joueurs

---
+++ London/src/fr/m1miage/london/Regles.java
@@ -0,0 +1,16 @@
+package fr.m1miage.london;
+
+public class Regles {
+	public final static int PTPAUVRETE = 5;
+	public final static int ARGENT = 10;
+	public final static int NBMAXCARTES=9;
+	public final static int NBCARTESDEPART=5;
+	public final static int NBMAXJOUEURS=5;
+	
+	public Regles(){
+		
+	}
+	
+	
+
+}
