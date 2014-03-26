4a8c4ef0ad43c600828bbd4d949e71cb1cc42913
merlier <arnaud.merlier2@gmail.com>
2014-03-26 15:04:22 +0100

    modif des tests

--- London/src/test/CarteTest.java
+++ London/src/test/CarteTest.java
@@ -10,6 +10,7 @@ public class CarteTest {
 
 	@Test
 	public void testToString() {
+		// modif des test à faire
 		Carte c = new Carte(1,"nom","A",2,"Rouge",1,1,1,"toto",1,true,1,1,1,1);
 		String affichage = "id : 1\nnom : nom\nperiode : A\n";		
 		assertEquals(affichage, c.toString());
