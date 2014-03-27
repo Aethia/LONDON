213327a90ab4d8d412450c511f6dd3a6ed140f3b
nownows <arnaud.merlier2@gmail.com>
2014-03-27 15:27:31 +0100

    t

---
+++ London/src/test/QuartierTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fr.m1miage.london.classes.Quartier;
+
+public class QuartierTest {
+
+	@Test
+	public void testGetQuartiersAdjacents() {
+		Quartier q = new Quartier();
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetQuartiersAdjacents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAuSudTamise() {
+		fail("Not yet implemented");
+	}
+
+}
