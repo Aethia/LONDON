9a68d3de6ee6ca3c806706f24f628b7b7ee269c5
Myriam <ethilanie@gmail.com>
2014-04-01 14:03:58 +0200

    Le plateau comporte désormais un étalage

--- London/src/fr/m1miage/london/classes/Plateau.java
+++ London/src/fr/m1miage/london/classes/Plateau.java
@@ -3,16 +3,19 @@ package fr.m1miage.london.classes;
 import java.util.HashMap;
 import java.util.Map;
 
+import fr.m1miage.london.Regles;
 import fr.m1miage.london.db.QuartiersManager;
 
 public class Plateau {
 	private Map<Integer,Quartier> quartiers = new HashMap<Integer, Quartier>();
+	private Etalage etalage;
 	
 	public Plateau(){
 		
 	}
 	public void init(){
 		quartiers = QuartiersManager.getQuartiers();
+		etalage = new Etalage(Regles.NBMAXJOUEURS);
 	}
 	
 	public Map<Integer, Quartier> getQuartiers() {
