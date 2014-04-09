69d885dc4287dab18ef868762647187a359c97a1
maximus <milletm54@gmail.com>
2014-04-09 16:07:16 +0200

    update invest()

--- London/src/test/JoueurTest.java
+++ London/src/test/JoueurTest.java
@@ -1,6 +1,7 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
 import java.awt.Color;
 
@@ -8,12 +9,14 @@ import org.junit.Test;
 
 import fr.m1miage.london.classes.Carte;
 import fr.m1miage.london.classes.Joueur;
+import fr.m1miage.london.classes.Pioche;
+import fr.m1miage.london.classes.Plateau;
 
 public class JoueurTest {
 
 	@Test
 	/*
-	 * Test sur le getMain qui doit retourner une copie de la main et non la main en elle m�me
+	 * Test sur le getMain qui doit retourner une copie de la main et non la main en elle m�me
 	 */
 	public void testGetMain(){
 		Joueur j = new Joueur(1,"toto",Color.black);
@@ -23,5 +26,27 @@ public class JoueurTest {
 		j.ajouterCarteMain(c1);
 		assertNotSame(j.getMainDuJoueur(), j.getMainDuJoueurCopie());
 	}
+	
+	@Test
+	public void testInvestirArgent(){
+		Joueur j = new Joueur(1,"toto",Color.black);
+		Plateau pl = new Plateau();
+		Pioche p = new Pioche();
+		pl.init();
+		p.init();
+		assertEquals("Désolé vous ne pouvez pas investir dans ce quartier !",j.invest(1,pl,p));
+	}
+	
+	@Test
+	public void testInvestirQuartier(){
+		Joueur j = new Joueur(1,"toto",Color.black);
+		Plateau pl = new Plateau();
+		Pioche p = new Pioche();
+		pl.init();
+		p.init();
+		
+		
+	}
+	
 
 }
