c591cb71115673a028ae4648fa3ae2c53a98ebf7
Aethia <elise.richard@live.fr>
2014-04-15 20:21:34 +0200

    Fix Test Rémy

--- London/test/fr/m1miage/london/classes/PartieTest.java
+++ London/test/fr/m1miage/london/classes/PartieTest.java
@@ -1,6 +1,6 @@
 package fr.m1miage.london.classes;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.awt.Color;
 import java.util.ArrayList;
@@ -21,10 +21,10 @@ public class PartieTest {
 	private List<Joueur> listeJoueurs;
 	private Quartier q1;
 	private ZoneConstruction zc;
-	
+
 	@Before
 	public void setUp() throws Exception{
-		
+
 		listeJoueurs = new ArrayList<Joueur>();
 		pl = new Plateau();
 		j = new Joueur(1,"toto",Color.black);
@@ -39,7 +39,7 @@ public class PartieTest {
 		q1 = new Quartier();	
 		zc = new ZoneConstruction();
 	}
-	
+
 	/*
 	 * testCalculPointsVictoire
 	 */
@@ -47,25 +47,25 @@ public class PartieTest {
 	public void testCalculPointsVictoireRembourseEmprunt() {
 		j.setAddPoint_victoire(10);
 		j.emprunter(10);
-		
+
 		p.calculPointsVictoire();
 		assertEquals(10, j.getPoint_victoire());
 	}
-	
+
 	@Test
 	public void testCalculPointsVictoireRemboursePasEmprunt() {
 		j.setAddPoint_victoire(10);
 		j.emprunter(10);
 		j.setArgent(10);
-		
+
 		p.calculPointsVictoire();
 		assertEquals(6, j.getPoint_victoire());
 	}
-	
+
 	@Test
 	public void testCalculPointsVictoireCarteEnMain() {
 		//on a 6 cartes en main au départ
-		
+
 		j.ajouterCarteMain(c);
 		j.ajouterCarteMain(c1);
 		j.ajouterCarteMain(c2);
@@ -73,7 +73,7 @@ public class PartieTest {
 		p.calculPointsVictoire();
 		assertEquals(9, j.getPoint_pauvrete());
 	}
-	
+
 	@Test
 	public void testCalculPointsVictoireTrancheArgent() {
 		j.setAddPoint_victoire(10);
@@ -81,7 +81,7 @@ public class PartieTest {
 		p.calculPointsVictoire();
 		assertEquals(13, j.getPoint_victoire());
 	}
-	
+
 	@Test
 	public void testCalculPointsVictoirePasTrancheArgent() {
 		j.setAddPoint_victoire(10);
@@ -89,7 +89,7 @@ public class PartieTest {
 		p.calculPointsVictoire();
 		assertEquals(11, j.getPoint_victoire());
 	}
-	
+
 	//arrondissement
 	//carte zone de construct ou pas
 	@Test
@@ -97,7 +97,12 @@ public class PartieTest {
 		q1.setId(1);
 		q1.setProprietaireQuartier(j);
 		q1.setPoint_victoire(10);
-		
+
+		Plateau plateau = new Plateau();
+		plateau.getQuartiers().put(1, q1);
+
+		p.setPlateau(plateau);
+
 		p.calculPointsVictoire();
 		//assertEquals(j, q1.getProprietaireQuartier());
 		assertEquals(11, j.getPoint_victoire());
@@ -105,7 +110,7 @@ public class PartieTest {
 
 	@Test
 	public void testCalculPointsVictoireArrondissementMetro() {
-		
+
 		q1.setId(1);
 		q1.setProprietaireQuartier(j);
 		q1.setPoint_victoire(10);
@@ -122,7 +127,7 @@ public class PartieTest {
 		zc.addPile(c);
 		zc.addPile(c1);
 		zc.ajouterCarte(1, c2);
-		
+
 		p.calculPointsVictoire();
 		assertEquals(36, j.getPoint_victoire());
 	}
@@ -131,17 +136,17 @@ public class PartieTest {
 	 */
 	@Test
 	public void testCalculGagnantDefaussePauvre() {
-		
+
 		j.setAddPoint_pauvrete(20);
 		j1.setAddPoint_pauvrete(30);
 		p.calculGagnant();
 		assertEquals(10, j1.getPoint_pauvrete());
 	}
-	
+
 	@Test
 	public void testCalculGagnantTableauPoints() {
 		j1.setAddPoint_victoire(20);
-		
+
 		j.setAddPoint_pauvrete(20);
 		j1.setAddPoint_pauvrete(35);
 		p.calculGagnant();
@@ -154,10 +159,10 @@ public class PartieTest {
 		j.setAddPoint_pauvrete(5);
 		j1.setAddPoint_pauvrete(5);
 		q1.setProprietaireQuartier(j1);
-		
+
 		p.calculGagnant();
 		assertEquals(j1, listeJoueurs.get(0));
 	}
-	
+
 
 }
