82bd91d9333aa63053183fca0573889139e24476
merlier <arnaud.merlier2@gmail.com>
2014-04-16 14:35:55 +0200

    ajout listener reseau (ouais je suis sur master, j'ai oublié de creer la branche)

--- London/src/fr/m1/miage/london/network/client/Reception.java
+++ London/src/fr/m1/miage/london/network/client/Reception.java
@@ -2,16 +2,25 @@ package fr.m1.miage.london.network.client;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.m1.miage.london.network.IncomingListenerClient;
 
 
 public class Reception implements Runnable {
 
 	private BufferedReader in;
 	private String message = null;
+	List<IncomingListenerClient> listeners = new ArrayList<IncomingListenerClient>();
 	
 	public Reception(BufferedReader in){
 		
 		this.in = in;
+	}
+	
+	public void addListener(IncomingListenerClient toAdd){
+		listeners.add(toAdd);
 	}
 	
 	public void run() {
@@ -20,7 +29,11 @@ public class Reception implements Runnable {
 	        try {
 	        	
 			message = in.readLine();
+			for (IncomingListenerClient list : listeners){
+				list.nouveauMessage(message);
+			}
 			System.out.println(message);
+			
 			
 		    } catch (IOException e) {
 				
