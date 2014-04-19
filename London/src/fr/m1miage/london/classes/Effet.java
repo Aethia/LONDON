fb092790e524eebd2ee10307a3be5be62d89b8e3
Rémy <remy.marsan@gmail.com>
2014-04-19 19:19:59 +0200

    Effets des cartes 19, 106, 110, 37, 63, 50, 62, 94, 91, 93, 83, 54, 67, 57, 5 Correspondant aux effets 5, 7, 11, 25, 23, 12, 15 de effet.xml
    + tests pour chaque effet

--- London/src/fr/m1miage/london/classes/Effet.java
+++ London/src/fr/m1miage/london/classes/Effet.java
@@ -1,6 +1,11 @@
 package fr.m1miage.london.classes;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Map;
+
+import fr.m1miage.london.GestionErreurs;
+import fr.m1miage.london.Partie;
 
 public class Effet implements Serializable{
 	private int idEffet;
@@ -42,6 +47,99 @@ public class Effet implements Serializable{
 		return msg.toString();
 	}
 
+	//cartes 19, 106, 110
+	//effet 5
+	//on pioche 2 cartes
+	public GestionErreurs prendreDeuxCartes(Pioche pioche, Joueur j){
+		if(pioche.getNbCartes() >= 2){
+			j.ajouterCartesMain(pioche.tirerNCartes(2));
+			return GestionErreurs.NONE;
+		}
+		else
+			return GestionErreurs.NOT_ENOUGH_CARD;
+	}
 	
+	//cartes 37, 63
+	//effet 7
+	//on reçoit un point de victoire pour chaque carte non brune dans la zone de construction
+	public void pVPourCartesNonBrune(Joueur j){
+		//for(int i = 0; i < j.getZone_construction().getNbPiles(); i++){
+			//ArrayList<Carte> pile= j.getZone_construction().getCartesPile(i);
+		
+		ArrayList<ArrayList<Carte>> zc = j.getZone_construction().getCartes();
+		for(ArrayList<Carte> pile : zc){
+				
+			for(Carte c: pile){
+				if(!c.getCouleur().equals("Brun"))
+					j.setAddPoint_victoire(1);
+			}
+		}
+	}
 	
+	//cartes 50, 62, 94
+	//effet 11
+	//on reçoit un point de victoire pour chaque carte brune dans la zone de construction
+	public void pVPourCartesBrune(Joueur j){
+		ArrayList<ArrayList<Carte>> zc = j.getZone_construction().getCartes();
+		for(ArrayList<Carte> pile : zc){
+			for(Carte c: pile){
+				if(c.getCouleur().equals("Brun"))
+					j.setAddPoint_victoire(1);
+			}
+		}
+	}
+	
+	//cartes 91, 93
+	//effet 25
+	//£2 pour chaque quartier au nord de la tamise
+	public void argentQuartiersNord(Plateau plateau, Joueur j){
+		Map<Integer,Quartier> quartiers = plateau.getQuartiers();
+		for(Integer key : quartiers.keySet()){
+			if( ( quartiers.get(key).getProprietaireQuartier() == j ) && ( !quartiers.get(key).isAuSudTamise() ) ) {
+				j.setAddArgent(2);
+			}
+		}
+	}
+	
+	//cartes 83
+	//effet 23
+	//£2 pour chaque quartier au sud de la tamise
+	public void argentQuartiersSud(Plateau plateau, Joueur j){
+		Map<Integer,Quartier> quartiers = plateau.getQuartiers();
+		for(Integer key : quartiers.keySet()){
+			if( ( quartiers.get(key).getProprietaireQuartier() == j ) && ( quartiers.get(key).isAuSudTamise() ) ) {
+				j.setAddArgent(2);
+			}
+		}
+	}
+	
+	//cartes 54, 67
+	//effet 12
+	//£1 pour chaque quartier occupé
+	public void argentQuartiersOccupes(Plateau plateau, Joueur j){
+		Map<Integer,Quartier> quartiers = plateau.getQuartiers();
+		for(Integer key : quartiers.keySet()){
+			if(quartiers.get(key).getProprietaireQuartier() == j){
+				j.setAddArgent(1);
+			}
+		}
+	}
+	
+	//cartes 57, 59
+	//effet 15
+	//£2 pour chaque quartier adjacent à la tamise
+	public void argentQuartiersAdjacentsTamise(Plateau plateau, Joueur j){
+		Map<Integer,Quartier> quartiers = plateau.getQuartiers();
+		for(Integer key : quartiers.keySet()){
+			if(quartiers.get(key).getProprietaireQuartier() == j){
+				boolean position = quartiers.get(key).isAuSudTamise();
+				for(Quartier q : quartiers.get(key).getQuartiersAdjacents()){
+					if(q.isAuSudTamise() != position){
+						j.setAddArgent(2);
+						break;
+					}
+				}
+			}
+		}
+	}
 }
---
+++ London/test/fr/m1miage/london/classes/EffetTest.java
@@ -0,0 +1,224 @@
+package fr.m1miage.london.classes;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.m1miage.london.Partie;
+
+public class EffetTest {
+	
+	private Joueur j;
+	private Carte c, c1, c2;
+	private ZoneConstruction zc;
+	private Pioche pioche;
+	private Partie p;
+	private Effet effet;
+	private List<Joueur> listeJoueurs;
+	private Quartier q1, q2, q3;
+	private Plateau plateau;
+	
+	private Set<Quartier> quartiersAdjacents1;
+	private Set<Quartier> quartiersAdjacents2;
+	
+	@Before
+	public void setUp() throws Exception{                                             
+		p = new Partie();
+		effet = new Effet();
+		zc = new ZoneConstruction();
+		pioche = new Pioche();
+		//p.setPioche(pioche);
+		j = new Joueur(1,"toto",Color.black);
+		j.setZoneConstruction(zc);
+		c = new Carte(1,"nom","A",2,"Brun",null);
+		c1 =new Carte(2,"nom","A",2,"Bleu",null);
+		c2 =new Carte(3,"nom","A",2,"Bleu",null);
+		q1 = new Quartier();
+		q2 = new Quartier();
+		q3 = new Quartier();
+		plateau = new Plateau();
+		
+		p.setPlateau(plateau);
+	}
+	
+	@Test
+	public void testPrendreDeuxCartes() {
+		pioche.ajouterCarte(c);
+		pioche.ajouterCarte(c1);
+		pioche.ajouterCarte(c2);
+		
+		effet.prendreDeuxCartes(pioche, j);
+		assertEquals(2, j.getNb_cartes());
+	}
+	
+	@Test
+	public void testPrendreDeuxCartesPiocheVide() {
+		
+		effet.prendreDeuxCartes(pioche, j);
+		assertEquals(0, j.getNb_cartes());
+	}
+
+	@Test
+	public void testPVPourCartesNonBrune() {
+		zc.addPile(c);
+		zc.addPile(c1);
+		zc.addPile(c2);
+		
+		effet.pVPourCartesNonBrune(j);
+		assertEquals(2, j.getPoint_victoire());
+		
+	}
+	
+	@Test
+	public void testPVPourCartesNonBrunePiocheVide() {
+		
+		effet.pVPourCartesNonBrune(j);
+		assertEquals(0, j.getPoint_victoire());
+		
+	}
+	
+	@Test
+	public void testPVPourCartesBrune() {
+		zc.addPile(c);
+		zc.addPile(c1);
+		zc.addPile(c2);
+		
+		effet.pVPourCartesBrune(j);
+		assertEquals(1, j.getPoint_victoire());
+		
+	}
+	
+	@Test
+	public void testPVPourCartesBruneAucune() {
+		zc.addPile(c1);
+		zc.addPile(c2);
+		
+		effet.pVPourCartesBrune(j);
+		assertEquals(0, j.getPoint_victoire());
+		
+	}
+	
+	@Test
+	public void testArgentQuartiersNordFaux() {
+		q1.setId(1);
+		q1.setProprietaireQuartier(j);
+		q1.setAuSudTamise(true);
+		plateau.getQuartiers().put(1, q1);
+		
+		effet.argentQuartiersNord(plateau, j);
+		assertEquals(5, j.getArgent());
+		
+	}
+	
+	@Test
+	public void testArgentQuartiersNordVrai() {
+		q1.setId(1);
+		q1.setProprietaireQuartier(j);
+		q1.setAuSudTamise(false);
+		plateau.getQuartiers().put(1, q1);
+		
+		effet.argentQuartiersNord(plateau, j);
+		assertEquals(7, j.getArgent());
+		
+	}
+
+	@Test
+	public void testArgentQuartiersSudFaux() {
+		q1.setId(1);
+		q1.setProprietaireQuartier(j);
+		q1.setAuSudTamise(false);
+		plateau.getQuartiers().put(1, q1);
+		
+		effet.argentQuartiersSud(plateau, j);
+		assertEquals(5, j.getArgent());
+		
+	}
+	
+	@Test
+	public void testArgentQuartiersSudVrai() {
+		q1.setId(1);
+		q1.setProprietaireQuartier(j);
+		q1.setAuSudTamise(true);
+		plateau.getQuartiers().put(1, q1);
+		
+		effet.argentQuartiersSud(plateau, j);
+		assertEquals(7, j.getArgent());
+		
+	}
+	@Test
+	public void testArgentQuartiersOccupes(){
+		q1.setId(1);
+		q1.setProprietaireQuartier(j);
+		q2.setId(2);
+		q2.setProprietaireQuartier(j);
+		plateau.getQuartiers().put(1, q1);
+		plateau.getQuartiers().put(2, q2);
+		
+		effet.argentQuartiersOccupes(plateau, j);
+		assertEquals(7, j.getArgent());
+	}
+	
+	@Test
+	public void testArgentQuartiersOccupesAucun(){
+		
+		effet.argentQuartiersOccupes(plateau, j);
+		assertEquals(5, j.getArgent());
+	}
+	
+	@Test
+	public void testArgentQuartiersAdjacentsTamise(){
+		q1.setId(1);
+		q1.setProprietaireQuartier(j);
+		q1.setAuSudTamise(false);
+		q2.setId(2);
+		q2.setAuSudTamise(false);
+		q3.setId(3);
+		q3.setProprietaireQuartier(j);
+		q3.setAuSudTamise(true);
+		
+		quartiersAdjacents1 = new HashSet<Quartier>();
+		quartiersAdjacents1.add(q3);
+		q1.setQuartiersAdjacents(quartiersAdjacents1);
+		
+		quartiersAdjacents2 = new HashSet<Quartier>();
+		quartiersAdjacents2.add(q1);
+		q3.setQuartiersAdjacents(quartiersAdjacents2);
+		
+		plateau.getQuartiers().put(1, q1);
+		plateau.getQuartiers().put(2, q2);
+		plateau.getQuartiers().put(2, q3);
+		
+		effet.argentQuartiersAdjacentsTamise(plateau, j);
+		assertEquals(9, j.getArgent());
+	}	
+	
+	@Test
+	public void testArgentQuartiersAdjacentsTamiseAucunAdjacent(){
+		q1.setId(1);
+		q1.setProprietaireQuartier(j);
+		q1.setAuSudTamise(false);
+		q2.setId(2);
+		q2.setProprietaireQuartier(j);
+		q2.setAuSudTamise(false);
+		plateau.getQuartiers().put(1, q1);
+		plateau.getQuartiers().put(2, q2);
+		
+		effet.argentQuartiersAdjacentsTamise(plateau, j);
+		assertEquals(5, j.getArgent());
+	}
+	
+	@Test
+	public void testArgentQuartiersAdjacentsTamisePossedeAucun(){
+		
+		effet.argentQuartiersAdjacentsTamise(plateau, j);
+		assertEquals(5, j.getArgent());
+	}
+
+
+}
